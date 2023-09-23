0dd0ccc95553410df72fcdf32dbf3266eb2c801e
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-09-23 13:05:13 +0700

    Add files via upload

---
+++ Pra-exam/daily routine/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const taskList = document.getElementById("task-list");
+    const newTaskInput = document.getElementById("new-task");
+    const addTaskButton = document.getElementById("add-task");
+
+    addTaskButton.addEventListener("click", function() {
+        const taskText = newTaskInput.value.trim();
+        if (taskText !== "") {
+            addTask(taskText);
+            newTaskInput.value = "";
+        }
+    });
+
+    newTaskInput.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+
+    function addTask(taskText) {
+        const taskItem = document.createElement("li");
+        taskItem.textContent = taskText;
+
+        // Tambahkan tombol delete ke setiap tugas
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-button");
+
+        deleteButton.addEventListener("click", function() {
+            taskList.removeChild(taskItem);
+        });
+
+        taskItem.appendChild(deleteButton);
+
+        taskItem.addEventListener("click", function() {
+            taskItem.classList.toggle("completed");
+        });
+
+        taskList.appendChild(taskItem);
+    }
+});
