194a14f8a9fee94d167eaa6a72cda0eeb8ca7184
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-10-26 08:34:38 +0700

    Add files via upload

--- RPL X B/pertemuan 10/conversion.js
+++ RPL X B/pertemuan 10/conversion.js
@@ -9,6 +9,31 @@ document.write("string: " + angkaString + "<br>");
 document.write("Integer: " + angkaInteger + "<br>" );
 document.write("Float: " + angkaFloat + "<br>");
 
+//coercion (implicit type conversion)
+var angka = 10;
+var string = "29";
+var hasil = angka + string;
+
+document.write(<h3>coercion</h3>);
+document.write("angka: " + angka + "<br>");
+document.write("string: " + string + "<br>");
+document.write("Hasil: " + hasil + "<br>");
+
+var boolean = true;
+var number = 42;
+var result = boolean + number;
+
+document.write("Boolean: " + boolean + "<br>");
+document.write("Number: " + number + "<br>");
+document.write("Result: " + result + "<br>");
+
+
+
+
+
+
+
+
 
 
 
