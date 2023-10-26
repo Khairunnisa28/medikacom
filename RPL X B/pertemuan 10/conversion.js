59b0e5c01c3a5b1891ca552fd6cc62fb9e84ac55
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-10-26 08:11:01 +0700

    Add files via upload

---
+++ RPL X B/pertemuan 10/conversion.js
@@ -0,0 +1,14 @@
+var angkaString = '42';
+// merubah menjadi integer
+var angkaInteger = parseInt(angkaString);
+// merubah menjadi float (desimal)
+var angkaFloat = parseFloat(angkaString);
+
+document.write("<h3>Conversion</h3>");
+document.write("string: " + angkaString + "<br>");
+document.write("Integer: " + angkaInteger + "<br>" );
+document.write("Float: " + angkaFloat + "<br>");
+
+
+
+
